722dd2bb028e477661bf6a7bc89347c601dac3ef
HaganeKen <ktat73@csu.fullerton.edu>
2019-10-04 01:34:40 -0700

    Github upload

---
+++ app/src/main/java/com/example/a411assignment1_kentat/MainActivity.java
@@ -0,0 +1,19 @@
+/*
+Ken Tat
+CPSC411 Assignment 1
+T/Th 5:30PM
+*/
+
+package com.example.a411assignment1_kentat;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Assignment 1 CPSC411
+
+
+Ken Tat
+CPSC 411
+T/TH 5:30PM
+
+Assignment 1
